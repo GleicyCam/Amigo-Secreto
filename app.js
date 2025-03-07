22539c108d05d3b0f9ca8fc86b3fdeced1a3a1cb
Gleiciany Campos <eugleicianycampos@gmail.com>
2025-03-06 22:25:28 -0400

    função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let amigos = []; // Array para armazenar os nomes dos amigos
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo"); // Obtém o campo de entrada
+    const nome = input.value; // Obtém o nome e remove espaços extras
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome."); // Alerta se o campo estiver vazio
+    } 
+    else if (amigos.includes(nome)) {
+        alert(`O nome ${nome} já foi adicionado!`); // Alerta se o nome já estiver na lista
+    } 
+    else { 
+        amigos.push(nome); // Adiciona o nome ao array
+        atualizarLista("#listaAmigos", nome); // Atualiza a exibição da lista de amigos
+        input.value = ""; // Limpa o campo de entrada
+    }
