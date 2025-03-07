64a9543e39e16523129e637af4d09382b3bfecda
Gleiciany Campos <eugleicianycampos@gmail.com>
2025-03-06 22:30:08 -0400

    função para sortear os amigos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let amigos = []; // Array para armazenar os nomes dos amigos
+let sorteios = []; // Array para armazenar os sorteios realizados
 
 function adicionarAmigo() {
     const input = document.getElementById("amigo"); // Obtém o campo de entrada
@@ -22,4 +23,28 @@ function atualizarLista(tag, texto) {
     const item = document.createElement('li');
     item.textContent = texto;
     lista.appendChild(item);
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Nenhum amigo disponível para sortear."); // Verifica se há amigos na lista
+    }
+    
+    else if (sorteios.length === amigos.length) {
+        alert("Todos os amigos já foram sorteados!"); // Alerta que todos foram sorteados
+    }
+    else {
+    // Sorteia um amigo aleatório
+    const indiceSorteado = Math.floor(Math.random() * amigos.length); 
+    const amigoSorteado = amigos[indiceSorteado]; 
+
+    // Adiciona ao array de sorteios com a ordem
+    sorteios.push(amigoSorteado);
+
+    // Exibe o sorteio
+    const resultado = document.getElementById("resultado");
+
+    // Exibe a ordem do sorteio com nome correspondente
+    const ordem = sorteios.length;
+    resultado.innerHTML = `<p>O ${ordem} sorteado é: <strong>${amigoSorteado}</strong></p>`;
+    }
 }
